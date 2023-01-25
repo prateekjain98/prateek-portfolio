c67a8d69726cbd8443cb7c7fdb0ef9060fd59da3
Prateek Jain <prateek@evolvfit.in>
2023-01-25 23:05:20 +0000

    fix

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -25,7 +25,7 @@ export default function Projects({ projects }: Props) {
             <Carousel className="w-screen z-20" interval={null}>
                 {projects.map((project) => (
                     <Carousel.Item key={project._id} className="p-4">
-                        <div className="flex flex-col items-center mx-auto justify-center w-[80vw] md:w-[70vw] h-[68vh] p-2 mt-20">
+                        <div className="flex flex-col items-center mx-auto justify-center w-[80vw] md:w-[70vw] h-[68vh] p-2 md:mt-20">
                             <motion.img
                                 initial={{ opacity: 0 }}
                                 whileInView={{ opacity: 1 }}
